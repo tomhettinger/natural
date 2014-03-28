8cdc5840957520dd138f5a7aa02f6a7d16cc792b
tomhettinger <ssjsonic1@gmail.com>
2014-03-28 18:19:05 -0400

    fixed issue with httprequest not parsing due to wifi login html

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,17 +1,18 @@
 var locationOptions = { "timeout": 15000, "maximumAge": 60000 };  // Wait 15s for pos to return. Cache pos for 60s.
 
 
-
-function updateProgress(oEvent) {
-    if (oEvent.lengthComputable) {
-        var percentComplete = oEvent.loaded / oEvent.total;
-        console.log("JS: update progress = " + percentComplete);
-    }
+function isJSON(text) {
+    return (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').
+    replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+    replace(/(?:^|:|,)(?:\s*\[)+/g, '')));
 }
 
 
-function transferComplete(evt) {
-    console.log("JS: transfer complete.");
+function updateProgress(evt) {
+    if (evt.lengthComputable) {
+        var percentComplete = evt.loaded / evt.total;
+        console.log("JS: update progress = " + percentComplete);
+    }
 }
 
 
@@ -26,73 +27,71 @@ function transferCanceled(evt) {
 
 
 function locationSuccess(location) {
+    var tzOffset = new Date().getTimezoneOffset() * 60;
     var req = new XMLHttpRequest();
-    var url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&cnt=1"
-
-    // Experimental:
-    /* https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest */
-    req.ontimeout = function(e) {
-        console.log("JS: Timeout on httprequest.");
-    }
-
+    var url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&cnt=1";
+    req.ontimeout = function(e) {console.log("JS: Timeout on httprequest.");}
     req.addEventListener("progress", updateProgress, false);
-    req.addEventListener("load", transferComplete, false);
     req.addEventListener("error", transferFailed, false);
     req.addEventListener("abort", transferCanceled, false);
-
+    
     req.onload = function(e) {
-        if(req.readyState == 4 && req.status == 200) {
-            var response = JSON.parse(req.responseText);
-            var sunrise = response.sys.sunrise;
-            var sunset = response.sys.sunset;
+        console.log("JS: Transfer complete. readystate=" + req.readyState + " status=" + req.status);
 
-            console.log("JS: Sending weather.");
-            Pebble.sendAppMessage(
-                {"status": "reporting",
-                "sunrise": sunrise,
-                "sunset": sunset,
-                "tzOffset": new Date().getTimezoneOffset() * 60
+        // If request is succesful.
+        if (req.readyState == 4 && req.status == 200) {
+
+            // If text is in JSON format
+            if (isJSON(req.responseText)) {
+                console.log("JS: Parsing text...");
+                try {
+                    var response = JSON.parse(req.responseText);
+                } 
+                catch (e) {
+                    console.log("JS: Unable to convert text to JSON object.");
+                    Pebble.sendAppMessage( {"status": "failed", "tzOffset": tzOffset} );
+                    return;
                 }
-            );
+                var sunrise = response.sys.sunrise;
+                var sunset = response.sys.sunset;
+                console.log("JS: Sending weather...");
+                Pebble.sendAppMessage( {"status": "reporting", "sunrise": sunrise, "sunset": sunset, "tzOffset": tzOffset} );
+            }
+
+            else {
+                console.log('JS: Response text is not in JSON format.');
+                Pebble.sendAppMessage( {"status": "failed", "tzOffset": tzOffset} );
+            }
         }
 
         else {
             console.log("JS: Error communicating with Open Weather Map.");
-            Pebble.sendAppMessage(
-                {"status": "failed",
-                "tzOffset": new Date().getTimezoneOffset() * 60
-                }
-            );
+            Pebble.sendAppMessage( {"status": "failed", "tzOffset": tzOffset} );
         }
     }
 
     console.log("JS: Location found, getting weather...");
     req.open("GET", url, true);
-    // At this point, sometimes the code never makes it to either if() case or else() case.  Why?
-
     req.send(null);
 }
 
 
 function locationError(error) {
     console.log("JS: Failed to get coords: " + error.message + "\n");
-    Pebble.sendAppMessage(
-        {"status": "failed",
-        "tzOffset": new Date().getTimezoneOffset() * 60
-        }
-    );
+    var tzOffset = new Date().getTimezoneOffset() * 60;
+    Pebble.sendAppMessage( {"status": "failed", "tzOffset": tzOffset} );
 }
 
 
 function readyHandler(e) {
     console.log("JS: Ready.");
-    Pebble.sendAppMessage({"status": "ready"});
+    Pebble.sendAppMessage( {"status": "ready"} );
 }
 
 
 function receivedHandler(message) {
     if(message.payload.status == "retrieve") {
-        console.log("JS: Recieved status \"retrieve\", getting location...")
+        console.log("JS: Recieved status \"retrieve\", getting location...");
         window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
     }
 }
