8c2dc027bbc9bda13341c1dd7ba60269c2754440
tomhettinger <ssjsonic1@gmail.com>
2014-05-09 13:15:47 -0500

    working on submission form

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -110,7 +110,8 @@ function receivedHandler(message) {
 
 function showConfigurationHandler() {
     console.log("showing configuration");
-    Pebble.openURL('http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-js/configurable.html');  
+    //Pebble.openURL('http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-js/configurable.html');
+    Pebble.openURL('http://tomhettinger.github.com/natural/index.html');
 }
 
 function webviewclosedHandler(e) {
